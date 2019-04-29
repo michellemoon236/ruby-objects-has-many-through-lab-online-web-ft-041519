3b3d5067b6754924566bf55ae33b2e337fbf9fed
Michelle Moon <michellemoon236@gmail.com>
2019-04-29 01:55:08 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,4 +1,17 @@
 class Patient 
   
   attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end 
+  
+  def new_appointment(date, )
 end 
