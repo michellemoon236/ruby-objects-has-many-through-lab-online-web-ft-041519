4b5a8e15295378c58073b223c07331e1806e50f3
Michelle Moon <michellemoon236@gmail.com>
2019-04-29 02:01:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,13 +15,13 @@ class Song
     @@all
   end
   
-  def genre
-    @genre
-  end
+  # def genre
+  #   @genre
+  # end
   
-  def artist
-    @artist
-  end
+  # def artist
+  #   @artist
+  # end
     
   
 end
