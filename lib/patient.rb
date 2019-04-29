f03a2af5a475b9b3dcea94cabb65b4730433a2ef
Michelle Moon <michellemoon236@gmail.com>
2019-04-29 01:58:28 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -18,7 +18,7 @@ class Patient
   end 
   
   def appointments 
-    Appointment.all.select |appointment|
+    Appointment.all.select do |appointment|
       appointment.patient = self 
     end
   end
